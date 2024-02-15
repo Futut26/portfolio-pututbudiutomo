ce5316e4593c7899b81edc4dc37289475b42923c
PututBudiutomo <pututbudiutomo2020@gmail.com>
2024-02-15 20:59:02 +0700

    hhh

---
+++ src/constants/index.js
@@ -0,0 +1,175 @@
+import { meta, shopify, starbucks, tesla, mp } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    php,
+    mysql,
+    uml,
+    laravel,
+    figma
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: php,
+        name: "PHP",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mysql,
+        name: "MYSQL",
+        type: "Frontend",
+    },
+    {
+        imageUrl: uml,
+        name: "UML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: laravel,
+        name: "LARAVEL",
+        type: "Frontend",
+    },
+    {
+        imageUrl: figma,
+        name: "FIGMA",
+        type: "Frontend",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+];
+
+export const experiences = [
+    {
+        title: "Full Stack Developer",
+        company_name: "MP Project",
+        icon: mp,
+        iconBg: "#accbe1",
+        date: "juni 2023 - Desember 2023",
+        points: [
+            "Developing and maintaining web applications using LARAVEL and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Futut26'
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://linkedin.com/in/putut-budiutomo46',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'stellar jardin residence housing marketing website',
+        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+        link: 'https://github.com/MP-Project-26/stellar_jardin',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'UI Design for Stellar Jardin Residence Housing',
+        description: 'Build UI Design for Stellar Jardin Residence Housing using Figma.',
+        link: 'https://www.figma.com/file/op3orT4PkKRqw1MwX3G9Vh/Jardin?type=design&node-id=0%3A1&mode=design&t=2YaCfvhARrFbwfhX-1',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Alpen Hills Residence Housing Marketing Website',
+        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+        link: 'https://github.com/MP-Project-26/alpen_hills',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'UI Design for Alpen Hills Residence Housing',
+        description: 'Build UI Design for Alpen Hills Residence Housing using Figma.',
+        link: 'https://www.figma.com/file/3KjzhlKq4S4RZ44KL8AwhL/AlpenDesign?type=design&node-id=0%3A1&mode=design&t=H6dp1zQ1oGbhVmIp-1',
+    }
+];
